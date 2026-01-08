250b519c04e269d1c0329863cb70469263ab60e3
Chikire <chikireakuibe@gmail.com>
2026-01-08 03:03:05 -0800

    added parameters and returns to the docstring

--- src/data_fixr/detect_anomalies.py
+++ src/data_fixr/detect_anomalies.py
@@ -4,5 +4,25 @@ def detect_anomalies(df, method):
     
     Outliers in a dataset can heavy impact our analysis negatively. This function helps to identify 
     potential anomalies in numeric columns of a Pandas DataFrame that is either Normally distributed 
-    or skewed. 
+    or skewed. It takes in a DataFrame, identifies numeric columns, and applies the specified method 
+    to flag outliers. The z-score method is suitable for normally distributed data but sensitive to 
+    extreme outliers, while the IQR method is better for Skewed distributions and robust to extreme 
+    values. Furthermore, the Z-score method dentifies points that are X standard deviations away from 
+    the mean. The IQR method identifies points that fall below Q1 - 1.5*IQR or above Q3 + 1.5*IQR.
+    
+Parameters
+    ----------
+   - df (pd.DataFrame): The Dataframe containing numeric columns to analyze for anomalies.
+    method (Z-Score or IQR): The method to use for anomaly detection. Choose 'Z-Score' for normally
+    distributed data or 'IQR' for skewed data.
+
+    Returns
+    -------
+    - pd.DataFrame: A DataFramme with the same structure as the input DataFrame, but only 
+        containing numeric columns with the outliers specified method and additional columns indicating that
+        the outliers have been flagged with a boolean value (True for outlier).
+    - float: The percentage of outliers detected across all numeric columns.
+    """
+    
+
     
